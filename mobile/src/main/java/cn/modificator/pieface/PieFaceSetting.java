1d1810de00a46a82e10ba3f838f349903fa637d9
modificator <yunshangcn@gmail.com>
2017-04-25 22:27:52 +0800

    -

--- mobile/src/main/java/cn/modificator/pieface/PieFaceSetting.java
+++ mobile/src/main/java/cn/modificator/pieface/PieFaceSetting.java
@@ -3,12 +3,7 @@ package cn.modificator.pieface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.mobvoi.android.common.MobvoiApiManager;
-import com.mobvoi.android.common.api.MobvoiApiClient;
-import com.mobvoi.android.wearable.Wearable;
-
 public class PieFaceSetting extends AppCompatActivity {
-    MobvoiApiClient mobvoiApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
